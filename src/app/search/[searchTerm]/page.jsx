dd863f28e402648136ef0940a16cc24c17def93a
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-11-06 00:00:10 +0500

    add

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -3,7 +3,7 @@ import Results from '@/components/Results';
 export default async function SearchPage({ params }) {
   const seachTerm = params.searchTerm;
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=${'7f3f38fefd566a86a94db2d7fabc5272'}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
   );
   const data = await res.json();
   const results = data.results;
